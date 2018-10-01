26e711aebf5eafd7bfac8b905b789c3f3fdf91c3
Andres <afarangurens@unal.edu.co>
2018-09-30 22:41:49 -0500

    Validations added

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,2 +1,8 @@
 class Dog < ApplicationRecord
+	validates :name, :age,:sex, :description, :interesets, presence: true
+	validates :name, length: { maximum: 100, too_long:"Pueden haber unicamente %´{count} caracteres" }
+	validates :age, length: { in: 1..3, too_long: "Se permiten máximo %´{count} caracteres" }
+	validates :sex, length: { is: 1, too_long: "Se permiten máximo %´{count} caracteres" }
+	validates :description, length: { maximum: 5000, too_long: "Se permiten máximo %´{count} caracteres" }
+	validates :interesets length: { is: 1 ,too_long: "Se permiten máximo %´{count} caracteres" }
 end
