15edfce8b282f3a290955286dbdbf16a9e2fd03c
fgmendezg <fgmendezg@unal.edu.co>
2018-10-30 23:02:11 -0500

    arreglo produccion

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,18 +89,21 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'example.com',
+    user_name:            'socialpetunal@gmail.com',
+    password:             'TGQ1HkA3nbrBsUGrOygi',
+    authentication:       'plain',
+    enable_starttls_auto: true 
+  }
+
+  config.action_mailer.default_url_options = { :host => 'https://socialped-back-unal.herokuapp.com' }
+
 end
-
-
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
-  address:              'smtp.gmail.com',
-  port:                 587,
-  domain:               'example.com',
-  user_name:            'socialpetunal@gmail.com',
-  password:             'TGQ1HkA3nbrBsUGrOygi',
-  authentication:       'plain',
-  enable_starttls_auto: true 
-}
